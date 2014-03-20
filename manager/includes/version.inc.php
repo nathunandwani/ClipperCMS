e52f9f23629a9940f254e5ae5020d33b51fe4da4
TimGS <timspe@gmail.com>
2014-03-20 12:49:57 +0000

    Update version info and changelog for 1.2.7

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -4,7 +4,7 @@ define('CMS_NAME', 'ClipperCMS');
 
 define('CMS_RELEASE_VERSION', '1.2.7');
 define('CMS_RELEASE_NAME', 'Leander');
-define('CMS_RELEASE_DATE', '25 Feb 2014');
+define('CMS_RELEASE_DATE', '20 Mar 2014');
 
 define('CMS_FULL_APPNAME', CMS_NAME.' '.CMS_RELEASE_NAME.' '.CMS_RELEASE_NAME.' ('.CMS_RELEASE_DATE.')');
 
