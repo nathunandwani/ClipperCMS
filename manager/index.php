549b19fb1527c86eb8a2bb8950808380b1bbf006
TimGS <timspe@gmail.com>
2014-10-31 11:14:04 +0000

    Sameorigin header

--- manager/index.php
+++ manager/index.php
@@ -67,6 +67,7 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+header("X-Frame-Options: SAMEORIGIN"); 
 header("X-UA-Compatible: IE=8"); // Datepicker needs fixing
 
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE);
