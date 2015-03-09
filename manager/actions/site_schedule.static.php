ceb50873292525e501ee1613486c19596ce5cb0b
Ciprian Badea <ciprian.badea@gmail.com>
2015-03-09 10:51:18 -0400

    more tables to use datatables

--- manager/actions/site_schedule.static.php
+++ manager/actions/site_schedule.static.php
@@ -2,12 +2,11 @@
 if(!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE != 'true') exit();
 
 /* if(!$modx->hasPermission('edit_document')) {
-	$e->setError(3);
-	$e->dumpError();
+    $e->setError(3);
+    $e->dumpError();
 } */
 ?>
 
-<script src="media/script/jquery.dataTables.min.js"></script>
 <h1><?php echo $_lang["site_schedule"]?></h1>
 
 <div class="sectionHeader"><?php echo $_lang["publish_events"]?></div><div class="sectionBody" id="lyr1">
@@ -15,32 +14,31 @@ if(!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE != 'true') exit();
 $sql = "SELECT id, pagetitle, pub_date FROM $dbase.`".$table_prefix."site_content` WHERE pub_date > ".time()." ORDER BY pub_date ASC";
 $rs = $modx->db->query($sql);
 $limit = $modx->db->getRecordCount($rs);
-if($limit<1) {
-	echo "<p>".$_lang["no_docs_pending_publishing"]."</p>";
+    echo "<p>".$_lang["no_docs_pending_publishing"]."</p>";
 } else {
 ?>
-  <table border="0" cellpadding="2" cellspacing="0"  class="sortabletable sortable-onload-3 rowstyle-even" id="table-1" width="100%">
+  <table id="schedule-publish-events">
     <thead>
-      <tr bgcolor="#CCCCCC">
-        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
-        <th class="sortable"><b><?php echo $_lang['id'];?></b></th>
-        <th class="sortable"><b><?php echo $_lang['publish_date'];?></b></th>
+      <tr>
+        <th><b><?php echo $_lang['resource'];?></b></th>
+        <th width="5%"><b><?php echo $_lang['id'];?></b></th>
+        <th><b><?php echo $_lang['publish_date'];?></b></th>
       </tr>
     </thead>
     <tbody>
 <?php
-	for ($i=0;$i<$limit;$i++) {
-		$row = $modx->db->getRow($rs);
+    for ($i=0;$i<$limit;$i++) {
+        $row = $modx->db->getRow($rs);
 ?>
     <tr>
       <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle']?></a></td>
-	  <td><?php echo $row['id'] ;?></td>
+      <td><?php echo $row['id'] ;?></td>
       <td><?php echo $modx->toDateFormat($row['pub_date']+$server_offset_time)?></td>
     </tr>
 <?php
-	}
+    }
 ?>
-	</tbody>
+    </tbody>
 </table>
 <?php
 }
@@ -55,31 +53,31 @@ $sql = "SELECT id, pagetitle, unpub_date FROM $dbase.`".$table_prefix."site_cont
 $rs = $modx->db->query($sql);
 $limit = $modx->db->getRecordCount($rs);
 if($limit<1) {
-	echo "<p>".$_lang["no_docs_pending_unpublishing"]."</p>";
+    echo "<p>".$_lang["no_docs_pending_unpublishing"]."</p>";
 } else {
 ?>
   <table id="schedule-unpublish-events">
     <thead>
-      <tr bgcolor="#CCCCCC">
-        <th class="sortable"><b><?php echo $_lang['resource'];?></b></th>
-        <th class="sortable"><b><?php echo $_lang['id'];?></b></th>
-        <th class="sortable"><b><?php echo $_lang['unpublish_date'];?></b></th>
+      <tr>
+        <th><b><?php echo $_lang['resource'];?></b></th>
+        <th width="5%"><b><?php echo $_lang['id'];?></b></th>
+        <th><b><?php echo $_lang['unpublish_date'];?></b></th>
       </tr>
     </thead>
     <tbody>
 <?php
-	for ($i=0;$i<$limit;$i++) {
-		$row = $modx->db->getRow($rs);
+    for ($i=0;$i<$limit;$i++) {
+        $row = $modx->db->getRow($rs);
 ?>
     <tr>
       <td><a href="index.php?a=3&id=<?php echo $row['id'] ;?>"><?php echo $row['pagetitle'] ;?></a></td>
-	  <td><?php echo $row['id'] ;?></td>
+      <td width="5%"><?php echo $row['id'] ;?></td>
       <td><?php echo $modx->toDateFormat($row['unpub_date']+$server_offset_time) ;?></td>
     </tr>
 <?php
-	}
+    }
 ?>
-	</tbody>
+    </tbody>
 </table>
 <?php
 }
@@ -93,12 +91,12 @@ $sql = "SELECT id, pagetitle, pub_date, unpub_date FROM $dbase.`".$table_prefix.
 $rs = $modx->db->query($sql);
 $limit = $modx->db->getRecordCount($rs);
 if($limit<1) {
-	echo "<p>".$_lang["no_docs_pending_pubunpub"]."</p>";
+    echo "<p>".$_lang["no_docs_pending_pubunpub"]."</p>";
 } else {
 ?>
   <table id="schedule-all-events">
     <thead>
-      <tr bgcolor="#CCCCCC">
+      <tr>
         <th><b><?php echo $_lang['resource'];?></b></th>
         <th width="5%"><b><?php echo $_lang['id'];?></b></th>
         <th width="20%"><b><?php echo $_lang['publish_date'];?></b></th>
@@ -107,19 +105,19 @@ if($limit<1) {
     </thead>
     <tbody>
 <?php
-	for ($i=0;$i<$limit;$i++) {
-		$row = $modx->db->getRow($rs);
+    for ($i=0;$i<$limit;$i++) {
+        $row = $modx->db->getRow($rs);
 ?>
     <tr>
-	<td><a href="index.php?a=3&id=<?php echo $row['id']?>"><?php echo $row['pagetitle']?></a></td>
-	<td><?php echo $row['id']?></td>
-	<td><?php echo $row['pub_date']==0 ? "" : $modx->toDateFormat($row['pub_date']+$server_offset_time)?></td>
-	<td><?php echo $row['unpub_date']==0 ? "" : $modx->toDateFormat($row['unpub_date']+$server_offset_time)?></td>
+    <td><a href="index.php?a=3&id=<?php echo $row['id']?>"><?php echo $row['pagetitle']?></a></td>
+    <td><?php echo $row['id']?></td>
+    <td><?php echo $row['pub_date']==0 ? "" : $modx->toDateFormat($row['pub_date']+$server_offset_time)?></td>
+    <td><?php echo $row['unpub_date']==0 ? "" : $modx->toDateFormat($row['unpub_date']+$server_offset_time)?></td>
     </tr>
 <?php
-	}
+    }
 ?>
-	</tbody>
+    </tbody>
 </table>
 <?php
 }
--- manager/media/script/manager.js
+++ manager/media/script/manager.js
@@ -1,12 +1,12 @@
 $(document).ready(function($) {
-	
+
 	$('#dob').datepicker({
 		changeMonth: true,
 		changeYear: true,
 		yearRangeType: 'c-90:c+90',
 		dateFormat: config.date_format
 	});
-	
+
 	if($('#blockeduntil').length) {
 		$( '#blockeduntil, #blockedafter').datetimepicker({
 			changeMonth: true,
@@ -16,7 +16,7 @@ $(document).ready(function($) {
 			timeFormat: config.time_format
 		});
 	}
-	
+
 	$( '#pub_date, #unpub_date, input[id^="tv"].DatePicker, #datefrom, #dateto').datetimepicker({
 		changeMonth: true,
 		changeYear: true,
@@ -27,9 +27,9 @@ $(document).ready(function($) {
 
 	//tabs and tab history
 	(function ($) {
-		
+
 		//see on document load for the way tabs are remebered
-		
+
 		$(".js-tabs").tabs({
 			activate: function( event, ui ) {
 				//set session storage with the latest selected tab
@@ -42,13 +42,13 @@ $(document).ready(function($) {
 				}
 			}
 		});
-		
+
 		//check if tabs are present and higlight the selected value in session storage if remember_last_tab is configured
 		if($(".js-tabs").length){
 			var tabsId = $(".js-tabs").attr('id');
 			//get session storage
 			var savedPanelId = 0;
-			
+
 			if(config.remember_last_tab != '0'){
 				savedPanelId = sessionStorage.getItem(tabsId);
 			} else {
@@ -58,13 +58,13 @@ $(document).ready(function($) {
 			if(savedPanelId){
 				//activate if only exists, other plugins like MM will like to try activate tabs created by them
 				if($('#'+tabsId+' a[href="#'+savedPanelId+'"]').length){
-					var index = $('#'+tabsId+' a[href="#'+savedPanelId+'"]').parent().index(); 
+					var index = $('#'+tabsId+' a[href="#'+savedPanelId+'"]').parent().index();
 					$(".js-tabs").tabs("option", "active", index);
 				}
-				
+
 			}
 		}
-		
+
 	}(jQuery));
 
 	$('#search-documents').dataTable({
@@ -77,17 +77,24 @@ $(document).ready(function($) {
 			{"bSortable": false}
 	     ]
 	});
-	
+
 	$('#schedule-all-events').dataTable({
-		"bJQueryUI": true,
-		"aaSorting": [[ 2, "desc" ]]
+		"bJQueryUI": true
 	});
-	
+
+	$('#schedule-publish-events').dataTable({
+		"bJQueryUI": true,
+		"aaSorting": [[ 2, "asc" ]]
+	});
 	$('#schedule-unpublish-events').dataTable({
 		"bJQueryUI": true,
 		"aaSorting": [[ 2, "asc" ]]
 	});
-	
+
+	$('#onlineusers').dataTable({
+		"bJQueryUI": true
+	});
+
 	$('.tooltip').tooltip();
 
 	//TODO: change to datatable ajax pagination - remove old type pagination
@@ -97,34 +104,34 @@ $(document).ready(function($) {
 		"bInfo": false,
 		"bJQueryUI": true
 	});
-	
+
 	$('.js-confirm-delete, .js-confirm-duplicate').click(function(e){
-		
+
 		var message = '';
-		
+
 		e.preventDefault();
-		
+
 		if($(this).hasClass('js-confirm-delete')){
 			message = temp_lang.confirm_delete;
 		}
-		
+
 		if($(this).hasClass('js-confirm-duplicate')){
 			message = temp_lang.confirm_duplicate;
 		}
-		
-		if(confirm(message) == true) {
+
+		if(confirm(message) === true) {
 			window.location.href= $(this).attr('href');
 			return true;
 		}
 		return false;
 	});
-	
+
 	$('.plugin-execution-order').sortable({
 		placeholder: "ui-state-highlight",
 		stop: function( event, ui ) {
-			var parent = $(ui.item).parent()
+			var parent = $(ui.item).parent();
 			var parentId = parent.attr('id');
-			
+
 			//make list to be send to form field
 			var list = [];
 			$(parent).find('li').each(function(i){
@@ -133,30 +140,29 @@ $(document).ready(function($) {
 			$('#list_' + parentId).val(list.join(','));
 		}
 	});
-	
+
 	$('#tv-sort-order').sortable({
 		placeholder: "ui-state-highlight",
 		stop: function( event, ui ) {
-		
-			var parent = $(ui.item).parent()
+
+			var parent = $(ui.item).parent();
 			//make list to be send to form field
 			var list = [];
 			$(parent).find('li').each(function(i){
 			   list.push($(this).attr('id'));
 			});
-			
+
 			$('#list').val(list.join(';'));
 		}
 	});
-	
-	
+
+
 
 });
 
 $(window).load(function () {
 
 	//called on load because MM added tabs will never get remebered because they are added after manager.js is called
-	
+
 
 });
-
